a106fdee7cd643911081eec484d5cc1bb1482b2a
江泽民 <2571851049@qq.com>
2017-06-29 12:08:27 +0800

    [(UTC +8:00) 2017-6-29] Fully changed DataPreprocessing.h

--- Data.h
+++ Data.h
@@ -1,4 +1,4 @@
-#ifndef __DATA__
+﻿#ifndef __DATA__
 #define __DATA__
 
 #include"iostream"
@@ -7,29 +7,29 @@
 #include"map"
 #include"set"
 #include"cmath"
+
 using namespace std;
 
-template<typename data> class Data
+template<typename data>
+struct Data
 {
-protected:
+    //save the amount of samples.
+    unsigned int dataSize;
 
-	//save the amount of samples.
-	unsigned int dataSize;
+    //save the amount of propertise.
+    unsigned int propertySize;
 
-	//save the amount of propertise.
-	unsigned int propertySize;
+    //save the X stringSet.
+    vector<vector<data>> X;
 
-	//save the X stringSet.
-	vector<vector<data>> X;
+    //save the Y stringSet.
+    vector<data> Y;
 
-	//save the Y stringSet.
-	vector<data> Y;
+    //save the value types of lable.
+    set<data> lableValues;
 
-	//save the value types of lable.
-	set<data> lableValues;
-
-	//save the value types of all attributes.
-	vector<set<data>> attributeValues;
+    //save the value types of all attributes.
+    vector<set<data>> attributeValues;
 };
 
 #endif //  !__DATA__
--- DataPreprocessing.h
+++ DataPreprocessing.h
@@ -1,4 +1,4 @@
-#ifndef __DATAPREPROCESSING__
+﻿#ifndef __DATAPREPROCESSING__
 #define __DATAPREPROCESSING__
 
 #include"iostream"
@@ -8,225 +8,268 @@
 #include"set"
 #include"cmath"
 #include"Data.h"
+#include <cassert>
+
 using namespace std;
 
-template<typename data> class DataPreprocessing : private Data<data>
+template<typename data>
+class DataPreprocessing
 {
 public:
-	//initial function. (MAIN PROCESS)
-	DataPreprocessing(vector<vector<data>> &dataSet);
+    enum class FixMethod
+    {
+        AverageFix,
+        MedianFix,
+        ModeFix
+    };
 
-	//to do discretization for dataSet.
-	void discretization(unsigned int splitPointAmount);
+    //initial function. (MAIN PROCESS)
+    DataPreprocessing(const vector<vector<data>> &dataSet);
 
-	//to fix the missing value in dataSet: "method: average/median/mode/custom".
-	void fixMissingValue(string method);
-	void fixMissingValue(string method, data fixValue);
+    //to do discretization for dataSet.
+    void discretization(unsigned int splitPointAmount);
 
-	//to standardize the dataSet's value. "type: "
-	void standardize();
+    //to fix the missing value in dataSet: "method: average/median/mode/custom".
+    void fixMissingValue(FixMethod method);
+    void fixMissingValue(FixMethod, const data& fixValue);
 
-	//to kill the missing value sample.
-	void killMissingValueSample();
+    //to standardize the dataSet's value. "type: "
+    void standardize();
+
+    //to kill the missing value sample.
+    void killMissingValueSample();
 
 private:
-	//save the max value of dataset.
-	vector<data> maxValues;
+    //save the max value of dataset.
+    vector<data> maxValues;
 
-	//save the min value of dataset.
-	vector<data> minValues;
+    //save the min value of dataset.
+    vector<data> minValues;
 
-	//save the average value of dataset.
-	vector<data> averageValues;
+    //save the average value of dataset.
+    vector<data> averageValues;
 
-	//save the median value of dataset.
-	vector<data> medianValues;
+    //save the median value of dataset.
+    vector<data> medianValues;
 
-	//save the mode value of dataset.
-	vector<data> modeValues;
+    //save the mode value of dataset.
+    vector<data> modeValues;
+
+    Data<data> basicData;
 };
 
 template<typename data>
-DataPreprocessing<data>::DataPreprocessing(vector<vector<data>> &dataSet)
+DataPreprocessing<data>::DataPreprocessing(const vector<vector<data>> &dataSet)
 {
-	DataPreprocessing::X = dataSet;
-	DataPreprocessing::dataSize = dataSet.size();
-	DataPreprocessing::propertySize = dataSet[0].size();
+    assert(!dataSet.empty());
 
-	vector<data> max(DataPreprocessing::propertySize);
-	vector<data> min(DataPreprocessing::propertySize);
-	vector<data> average(DataPreprocessing::propertySize);
-	vector<data> madian(DataPreprocessing::propertySize);
-	vector<data> mode(DataPreprocessing::propertySize);
+    basicData.X = dataSet;
+    basicData.dataSize = dataSet.size();
+    basicData.propertySize = dataSet[0].size();
 
-	//The occurrence number of each feature.
-	vector<vector<data>> medianList;
-	vector<map<data, unsigned int>> modeMap(DataPreprocessing::propertySize);
-	
-	for (auto it = X.begin()->begin(); it != X.begin()->end(); it++)
-	{
-		unsigned int index = it - X.begin()->begin();
-		max[index] = min[index] = average[index] = *it;
-		modeMap[index][*it]++;
-	}
+    vector<data> max(basicData.propertySize);
+    vector<data> min(basicData.propertySize);
+    vector<data> average(basicData.propertySize);
+    vector<data> mode(basicData.propertySize);
 
-	for (auto line = X.begin() + 1; line != X.end(); line++)
-	{
-		for (auto column = line->begin(); column != line->end(); column++)
-		{
-			unsigned int index = column - line->begin();
-			data tempData = *column;
+    //The occurrence number of each feature.
+    vector<map<data, unsigned int>> modeMap(basicData.propertySize);
 
-			if (max[index] < tempData)
-			{
-				max[index] = tempData;
-			}
+    for (auto it = basicData.X.begin()->begin();
+         it != basicData.X.begin()->end();
+         it++)
+    {
+        unsigned int index = it - basicData.X.begin()->begin();
+        max[index] = min[index] = average[index] = *it;
+        modeMap[index][*it]++;
+    }
 
-			if (min[index] > tempData)
-			{
-				min[index] = tempData;
-			}
+    for (auto row = basicData.X.begin() + 1;
+         row != basicData.X.end();
+         row++)
+    {
+        for (auto column = row->begin(); column != row->end(); column++)
+        {
+            unsigned int index = column - row->begin();
+            data tempData = *column;
 
-			average[index] += tempData;
+            if (max[index] < tempData)
+            {
+                max[index] = tempData;
+            }
 
-			modeMap[index][tempData]++;
-		}
-	}
+            if (min[index] > tempData)
+            {
+                min[index] = tempData;
+            }
 
-	for (auto &it = average.begin(); it != average.end(); it++)
-	{
-		*it /= dataSize;
-	}
+            average[index] += tempData;
 
-	for (auto it = modeMap.begin(); it != modeMap.end(); it++)
-	{
-		unsigned int index = it - modeMap.begin();
-		unsigned int count = 0;
-		data tempdata;
+            modeMap[index][tempData]++;
+        }
+    }
 
-		for (auto insideIt = it->begin(); insideIt != it->end(); insideIt++)
-		{
-			if (insideIt->second > count)
-			{
-				tempdata = insideIt->first;
-				count = insideIt->second;
-			}
-		}
+    for (auto it = average.begin(); it != average.end(); it++)
+    {
+        *it /= basicData.dataSize;
+    }
 
-		mode[index] = tempdata;
-	}
+    for (auto it = modeMap.begin(); it != modeMap.end(); it++)
+    {
+        unsigned int index = it - modeMap.begin();
+        unsigned int count = 0;
+        data tempdata;
 
-	DataPreprocessing::maxValues = max;
-	DataPreprocessing::minValues = min;
-	DataPreprocessing::averageValues = average;
-	DataPreprocessing::modeValues = mode;
+        for (auto insideIt = it->begin(); insideIt != it->end(); insideIt++)
+        {
+            if (insideIt->second > count)
+            {
+                tempdata = insideIt->first;
+                count = insideIt->second;
+            }
+        }
+
+        mode[index] = tempdata;
+    }
+
+    maxValues = max;
+    minValues = min;
+    averageValues = average;
+    modeValues = mode;
 }
 
 template<typename data>
 void DataPreprocessing<data>::discretization(unsigned int splitPointAmount)
 {
-	vector<data> thresholdValues(DataPreprocessing::propertySize);
-	for (unsigned int i = 0; i != DataPreprocessing::propertySize - 1; i++)
-	{
-		thresholdValues[i] = (DataPreprocessing::maxValues[i] - DataPreprocessing::minValues[i]) / splitPointAmount;
-	}
+    vector<data> thresholdValues(basicData.propertySize);
+    for (unsigned int i = 0; i != basicData.propertySize - 1; i++)
+    {
+        thresholdValues[i] =
+                (basicData.maxValues[i]
+                 - basicData.minValues[i]) / splitPointAmount;
+    }
 
-	for (auto &line = X.begin(); line != X.end(); line++)
-	{
-		for (auto &column = line->begin(); column != line->end(); column++)
-		{
-			unsigned int index = column - line->begin();
-			for (unsigned int count = 0; count != splitPointAmount; count++)
-			{
-				if (count * thresholdValues[index] < *column && *column < (count + 1) * thresholdValues[index])
-				{
-					*column = count;
-					break;
-				}
-			}
-		}
-	}
+    for (auto row = basicData.X.begin();
+         row != basicData.X.end(); row++)
+    {
+        for (auto column = row->begin(); column != row->end(); column++)
+        {
+            unsigned int index = column - row->begin();
+            for (unsigned int count = 0; count != splitPointAmount; count++)
+            {
+                if (count * thresholdValues[index] < *column
+                    && *column < (count + 1) * thresholdValues[index])
+                {
+                    *column = count;
+                    break;
+                }
+            }
+        }
+    }
 }
 
 template<typename data>
-void DataPreprocessing<data>::fixMissingValue(string method)
+void DataPreprocessing<data>::fixMissingValue(FixMethod method)
 {
-	vector<data> fixValues(DataPreprocessing::propertySize);
+    vector<data> fixValues(basicData.propertySize);
 
-	//method: average/median/mode/custom
-	if (method == "average")
-	{
-		fixValues = DataPreprocessing::averageValues;
-	}
+    //method: average/median/mode/custom
 
-	if (method == "mode")
-	{
-		fixValues = DataPreprocessing::modeValues;
-	}
+    switch (method)
+    {
+    case FixMethod::AverageFix:
+        fixValues = averageValues;
+        break;
 
-	for (auto &line = DataPreprocessing::X.begin(); line != DataPreprocessing::X.end(); line++)
-	{
-		for (auto &column = line->begin(); column != line->end(); column++)
-		{
-			unsigned int index = column - line->begin();
-			if (*column == 0 || *column == '\0')
-			{
-				*column = fixValues[index];
-			}
-		}
-	}
+    case FixMethod::MedianFix:
+        assert(0);
+        break;
+
+    case FixMethod::ModeFix:
+        fixValues = modeValues;
+        break;
+
+    default:
+        assert(0);
+    }
+
+    for (auto row = basicData.X.begin();
+         row != basicData.X.end();
+         row++)
+    {
+        for (auto column = row->begin(); column != row->end(); column++)
+        {
+            unsigned int index = column - row->begin();
+            if (*column == 0 || *column == '\0')
+            {
+                *column = fixValues[index];
+            }
+        }
+    }
 }
 
 template<typename data>
-void DataPreprocessing<data>::fixMissingValue(string method, data fixValue)
+void DataPreprocessing<data>::fixMissingValue(FixMethod,
+                                              const data &fixValue)
 {
-	for (auto &line = DataPreprocessing::X.begin(); line != DataPreprocessing::X.end(); line++)
-	{
-		for (auto &column = line->begin(); column != line->end(); column++)
-		{
-			if (*column == 0 || *column == '\0')
-			{
-				*column = fixValue;
-			}
-		}
-	}
+    for (auto row = basicData.X.begin();
+         row != basicData.X.end();
+         row++)
+    {
+        for (auto column = row->begin();
+             column != row->end();
+             column++)
+        {
+            if (*column == 0 || *column == '\0')
+            {
+                *column = fixValue;
+            }
+        }
+    }
 }
 
 
 template<typename data>
 void DataPreprocessing<data>::standardize()
 {
-	vector<double> ratio(DataPreprocessing::propertySize);
-	for (unsigned int i = 0; i != DataPreprocessing::propertySize; i++)
-	{
-		ratio[i] = 1 / static_cast<double>(DataPreprocessing::maxValues[i] - DataPreprocessing::minValues[i]);
-	}
+    vector<double> ratio(basicData.propertySize);
+    for (unsigned int i = 0; i != basicData.propertySize; i++)
+    {
+        ratio.push_back(
+                    1 / static_cast<double>(basicData.maxValues[i]
+                                            - basicData.minValues[i]));
+    }
 
-	for (auto &line = DataPreprocessing::X.begin(); line != DataPreprocessing::X.end(); line++)
-	{
-		for (auto &column = line->begin(); column != line->end(); column++)
-		{
-			unsigned int index = column - line->begin();
-			*column *= ratio[index];
-		}
-	}
+    for (auto row = basicData.X.begin();
+         row != basicData.X.end();
+         row++)
+    {
+        for (auto column = row->begin(); column != row->end(); column++)
+        {
+            unsigned int index = column - row->begin();
+            *column *= ratio[index];
+        }
+    }
 }
 
 template<typename data>
 void DataPreprocessing<data>::killMissingValueSample()
 {
-	for (auto &line = DataPreprocessing::X.begin(); line != DataPreprocessing::X.end(); line++)
-	{
-		for (auto &column = line->begin(); column != line->end(); column++)
-		{
-			if (*column == 0 || *column == '\0')
-			{
-				X.erase(line);
-				break;
-			}
-		}
-	}
+    for (auto row = basicData.X.begin();
+         row != basicData.X.end();
+         row++)
+    {
+        for (auto column = row->begin();
+             column != row->end();
+             column++)
+        {
+            if (*column == 0 || *column == '\0')
+            {
+                basicData.X.erase(row);
+                break;
+            }
+        }
+    }
 }
-
 
 #endif // !__DATAPREPROCESSING__
